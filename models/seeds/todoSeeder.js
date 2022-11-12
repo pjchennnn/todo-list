51b2734425db1fe7cf0bf1dc9ecd91057691eeb5
pjchennnn <coolkidtw1@gmail.com>
2022-11-12 17:02:14 +0800

    feat: add todo seeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const Todo = require('../todo')
 
+require('dotenv').config()
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
@@ -12,4 +14,9 @@ db.on('error', () => {
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
+
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: `name-${i}` })
+  }
+  console.log('done')
 })
